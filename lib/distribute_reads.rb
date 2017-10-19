d004093c12de9f16ea3e90db55317957dc4e5f46
Eric Hankins <ehankins@rednovalabs.com>
2017-10-19 11:36:13 -0500

    Remove global distribute_reads method

--- lib/distribute_reads.rb
+++ lib/distribute_reads.rb
@@ -1,7 +1,6 @@
 require "makara"
 require "distribute_reads/appropriate_pool"
 require "distribute_reads/cache_store"
-require "distribute_reads/global_methods"
 require "distribute_reads/version"
 
 module DistributeReads
@@ -40,21 +39,47 @@ module DistributeReads
     end
   end
 
-  # legacy
-  def self.default_to_primary
-    !by_default
-  end
+  def self.distribute_reads(**options)
+    raise ArgumentError, "Missing block" unless block_given?
 
-  # legacy
-  def self.default_to_primary=(value)
-    self.by_default = !value
+    unknown_keywords = options.keys - [:failover, :lag_failover, :lag_on, :max_lag, :primary]
+    raise ArgumentError, "Unknown keywords: #{unknown_keywords.join(", ")}" if unknown_keywords.any?
+
+    options = DistributeReads.default_options.merge(options)
+
+    previous_value = Thread.current[:distribute_reads]
+    begin
+      Thread.current[:distribute_reads] = {failover: options[:failover], primary: options[:primary]}
+
+      # TODO ensure same connection is used to test lag and execute queries
+      max_lag = options[:max_lag]
+      if max_lag && !options[:primary]
+        Array(options[:lag_on] || [ActiveRecord::Base]).each do |base_model|
+          if DistributeReads.lag(connection: base_model.connection) > max_lag
+            if options[:lag_failover]
+              # TODO possibly per connection
+              Thread.current[:distribute_reads][:primary] = true
+            else
+              raise DistributeReads::TooMuchLag, "Replica lag over #{max_lag} seconds#{options[:lag_on] ? " on #{base_model.name} connection" : ""}"
+            end
+          end
+        end
+      end
+      Rails.logger.debug 'Distributing reads!'
+      Rails.logger.debug '*'*80
+      value = yield
+      Rails.logger.debug '*'*80
+      warn "[distribute_reads] Call `to_a` inside block to execute query on replica" if value.is_a?(ActiveRecord::Relation) && !previous_value
+      value
+    ensure
+      Thread.current[:distribute_reads] = previous_value
+    end
   end
 end
 
 Makara::Proxy.send :prepend, DistributeReads::AppropriatePool
-Object.send :include, DistributeReads::GlobalMethods
-
-ActiveSupport.on_load(:active_job) do
-  require "distribute_reads/job_methods"
-  include DistributeReads::JobMethods
-end
+#
+# ActiveSupport.on_load(:active_job) do
+#   require "distribute_reads/job_methods"
+#   include DistributeReads::JobMethods
+# end
